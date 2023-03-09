8aa6e900c914d4c36ec7e92d8c44da66f55ecf51
Sergio Rodrigalvarez <sergio.rodrigalva@gmail.com>
2023-03-09 23:02:47 +0100

    chore: lint-staged test

--- src/entrypoints/online-business/controller/get-online-businesses.ts
+++ src/entrypoints/online-business/controller/get-online-businesses.ts
@@ -14,6 +14,7 @@ import {
 import {
     OnlineBusiness,
     OnlineBusinessName,
+    LINT_STAGED_TEST,
 } from 'src/modules/online-business/domain';
 import { Id } from 'src/modules/shared/domain';
 
